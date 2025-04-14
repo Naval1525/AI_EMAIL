53504e085d43e370075cb111995a162d7e168371
Aditya <adityamaurya.2807@gmail.com>
2025-04-15 03:24:25 +0530

    link

--- AI_EMAIL/backend/server.js
+++ AI_EMAIL/backend/server.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ai-email-frontend-taupe.vercel.app/",
+      "https://ai-email-frontend-taupe.vercel.app",
     ],
     credentials: true,
   })
